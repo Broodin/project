f25f36c7c6faa654cec603c514eda206db67d045
vipul_sawant <vipul_sawant@webserver-4894.vipul-sawant.dev.ash2.symcpe.com>
2016-10-14 12:15:05 +0000

    update

--- restapi.py
+++ restapi.py
@@ -1,7 +1,3 @@
-'''
-HTTP Post methods to get data from the monitor and saves in the database. 
-'''
-
 from flask import Flask, jsonify, request
 import pymysql,json
 from flask_restful import reqparse, abort, Api, Resource
@@ -16,26 +12,21 @@ api = Api(api_rest)
 parser = reqparse.RequestParser()
 
 
-#parses  application data from the url
+
 def app_parser():
 	
 	parser.add_argument('id')
 	parser.add_argument('name')
 	parser.add_argument('user')
+	parser.add_argument('type')
 	parser.add_argument('queue')
 	parser.add_argument('start_time')
 	parser.add_argument('finish_time')
 	parser.add_argument('state')
 	parser.add_argument('url')
 	parser.add_argument('elapsed_time')
-
-'''def heuristic_parser():
-	parser.add_argument('job_id')
-	parser.add_argument('h_name')
-	parser.add_argument('score')
-	parser.add_argument('severity')
-'''
-#parses cluster data from url	
+	parser.add_argument('checked')
+	
 def cluster_parser():
 	
 	parser.add_argument('q_name')
@@ -43,30 +34,20 @@ def cluster_parser():
 	parser.add_argument('max_capacity')
 	parser.add_argument('time')
 	
+def task_parser():
+	parser.add_argument('task_id')
+	parser.add_argument('app_id')
+	parser.add_argument('start_time')
+	parser.add_argument('finish_time')
+	parser.add_argument('type')
+	
+def taskcounter_parser():
+	parser.add_argument('task_id')
+	parser.add_argument('name')
+	parser.add_argument('value')
+	parser.add_argument('type')
 
-'''class Heuristic(Resource):
-	def get(self):
-		s = heuristic.select()
-		cur = s.execute()
-		#cur.execute("SELECT * FROM HEURISTIC")
-		rv=cur.fetchall()
-		return len(rv)
-
-	def post(self):
-		heuristic_parser()
-		args = parser.parse_args()
-		job_id=args['job_id']
-		h_name = args['h_name']
-		score = args['score']
-		severity = args['severity']
-		#stmt = heuristic.insert()
-		#stmt.execute(JOB_ID = job_id, H_NAME=h_name, SCORE = score, SEVERITY = severity)
-		#cur.execute("INSERT INTO HEURISTIC(JOB_ID,H_NAME,SCORE,SEVERITY) VALUES (%s,%s,%s,%s)",(job_id,h_name,score,severity))
-		#conn.commit()
-		return jsonify(job_id,h_name,score,severity)
-'''
-
-#Class to save application data	in database	
+	
 class Application(Resource):
 	def get(self):
 		s = application.select()
@@ -81,12 +62,14 @@ class Application(Resource):
 		id = args['id']
 		name = args['name']
 		user = args['user']
+		type= args['type']
 		queue = args['queue']
 		start_time = args['start_time']
 		finish_time = args['finish_time']
 		state = args['state']
 		url = args['url']
 		elapsed_time = args['elapsed_time']
+		flag = args['checked']
 		#stmt = application.insert()
 		#stmt.execute(ID = id, NAME = name, USER = user,QUEUE = queue,START_TIME = start_time, FINISH_TIME = finish_time, STATE = state, URL = url, ELAPSED_TIME = elapsed_time)
 		#cur.execute("INSERT INTO APPLICATION(ID,NAME,USER,QUEUE,START_TIME,FINISH_TIME,STATE,URL,ELAPSED_TIME) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",(id,name,user,queue,start_time,finish_time,state,url,elapsed_time))
@@ -94,8 +77,8 @@ class Application(Resource):
 		return jsonify(id,name,user,queue,start_time,finish_time,state,url,elapsed_time)
 
 
-#class to save cluster data in database		
-'''class Cluster(Resource):
+		
+class Cluster(Resource):
 	def get(self):
 		s = cluster.select()
 		cur = s.execute()
@@ -113,9 +96,33 @@ class Application(Resource):
 		#stmt = cluster.insert()
 		#stmt.execute(QUEUE_NAME = q_name,USED_CAPACITY = used_capacity, MAX_CAPACITY = max_capacity,DATE = time)
 		return jsonify(q_name,used_capacity,max_capacity,time)
-'''
+
+class Task(Resource):
+	def get(self):
+		return 0
+	def post(self):
+		task_parser()
+		args = parser.parse_args()
+		task_id = args['task_id']
+		app_id = args['app_id']
+		start_time = args['start_time']
+		finish_time = args['finish_time']
+		type = args['type']
+		return 1
+
+class TaskCounter(Resource):
+	def get(self):
+		return 0
+	def post(self):
+		taskcounter_parser()
+		args = parser.parse_args()
+		task_id = args['task_id']
+		name = args['name']
+		value = args['value']
+		type = args['type']
+		return 1
 		
 api.add_resource(Application, '/app/application/post')
-#api.add_resource(Heuristic, '/app/heuristic/post')
-#api.add_resource(Admin,'/url')
-#api.add_resource(Cluster, '/app/cluster/post')
+api.add_resource(Cluster, '/app/cluster/post')
+api.add_resource(Task, '/app/task/post')
+api.add_resource(TaskCounter, '/app/counter/post')
